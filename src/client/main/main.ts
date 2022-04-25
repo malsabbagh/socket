21aa8403f58df601e063d7688c126c185fecc9f5
Mohamad Alsabbagh <mohamad@alsabbagh.io>
2022-04-25 07:51:28 -0700

    Adding logic to close socket on beforeunload and adding ability to control that based on testCase query param

--- src/client/main/main.ts
+++ src/client/main/main.ts
@@ -3,6 +3,12 @@ const socketUrl = `wss://${baseUrl}/ws`;
 
 var socket = new WebSocket(socketUrl);
 
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams: URLSearchParams, prop: string) => searchParams.get(prop),
+});
+
+let testCase = (params as any).testCase || 'default';
+
 socket.onclose = (e) => {
     console.log("socket connection closed");
 };
@@ -15,26 +21,57 @@ socket.addEventListener("open", () => {
     }));
 });
 
-window.addEventListener("beforeunload", () => {
-    socket.send(JSON.stringify({
-        type: "close socket",
-        content: ["socket closed"]
-    }));
-});
-
-window.addEventListener("unload", () => {
-    if (socket.readyState == WebSocket.OPEN) {
-        socket.close();
-    }
-});
-
 // receive a message from the server
 socket.addEventListener("message", ({ data }) => {
     const packet = JSON.parse(data.toString());
-
     switch (packet.type) {
         case "message from server":
             console.log(packet.content);
             break;
     }
 });
+
+
+const defaultTestCase = () => {
+    window.addEventListener("beforeunload", () => {
+        console.log('beforeunload was called');
+        socket.send(JSON.stringify({
+            type: "close socket",
+            content: ["socket closed"]
+        }));
+    });
+
+    window.addEventListener("unload", () => {
+        console.log('Unload was called');
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.close();
+        }
+    });
+}
+
+const beforeUnloadTestCase = () => {
+    window.addEventListener("beforeunload", () => {
+        console.log('beforeunload was called');
+        socket.send(JSON.stringify({
+            type: "close socket",
+            content: ["socket closed"]
+        }));
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.close();
+        }
+    });
+
+    window.addEventListener("unload", () => {
+        console.log('Unload was called');
+    });
+}
+
+
+switch (testCase) {
+    case 'beforeUnloadSocketClose':
+        beforeUnloadTestCase();
+        break;
+    case 'default':
+    default:
+        defaultTestCase();
+}
